586d5c9eb19570d8df9de007beba2ae18a1b56e7
azemk <81404207+azemk@users.noreply.github.com>
2021-03-26 14:13:51 +0600

    Add files via upload

---
+++ index .php
@@ -0,0 +1,42 @@
+<html>
+    <style>
+        body{
+            font-size:18px;
+            background-color:rosybrown;
+            
+        }
+        .photo{
+            width:200px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            padding-bottom:100px;
+            
+        }
+        
+        
+
+
+
+    </style> 
+    <body>
+        
+        <h1>Profile</h1>
+         <img class="photo" src ="profile.png"/>
+         <hr>
+         <div class="td">
+            <tr>Name :</tr>
+            <td>Azem</td><br>
+            <tr>Surname :</tr>
+            <td>Kakitaeva</td><br>
+            <tr>Age  :</tr>
+            <td>17</td><br>
+            <tr>School:</tr>
+            <td>Vocational College Ala-Too</td><br>
+            <tr>Faculty:</tr>
+            <td>Computer Science </td><br>
+
+         </div>
+         <hr>
+    </body>
+</html>
